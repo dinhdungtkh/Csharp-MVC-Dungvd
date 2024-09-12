fea1c7add5fa7533e6307e692bfd3bba0a87a7d7
DinDun <van.dinhdung.tkh@gmail.com>
2024-09-12 15:56:11 +0700

    24 - Scenes Management in Unity3d

--- My project/Assets/Scripts/TestingScript.cs
+++ My project/Assets/Scripts/TestingScript.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TestingScript : MonoBehaviour
 {
-
+    public string nextSceneName ="Scene 2";
 
     private void Start()
     {
+        Invoke("MoveToSceneTwo", 3.0f);
     }
 
+    void MoveToSceneTwo()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
     private void OnEnable()
     {
         Debug.Log("OnEnable");
@@ -26,11 +32,11 @@ public class TestingScript : MonoBehaviour
     }
     private void Update()
     {
-        Debug.Log("Update is calling" + Time.deltaTime);
+        //Debug.Log("Update is calling" + Time.deltaTime);
     }
     private void FixedUpdate()
     {
-        Debug.Log("Fixupdate is calling " + Time.deltaTime);
+       // Debug.Log("Fixupdate is calling " + Time.deltaTime);
     }
 
 
