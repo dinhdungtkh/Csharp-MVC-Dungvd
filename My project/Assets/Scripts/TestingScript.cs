da89bc6c57dee59b6a97f355e7fc57b48ad6082d
DinDun <van.dinhdung.tkh@gmail.com>
2024-09-12 15:07:38 +0700

    OnEnable() and OnDisable() Functions in Unity 3D
    Awake - >  On Enable() -> FixedUpdate() -> Update() -> LateUpdate() ->
    On Application Quit > On Disable() -> On Destroy

--- My project/Assets/Scripts/TestingScript.cs
+++ My project/Assets/Scripts/TestingScript.cs
@@ -5,15 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class TestingScript : MonoBehaviour
 {
-    private void Start() {
+
+
+    private void Start()
+    {
     }
 
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+        Invoke("myMethod", 2.5f);
+    }
+    private void myMethod()
+    {
+        GameObject.Find("Plane").SetActive(false);
+    }
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
     private void Update()
     {
-        Debug.Log("Update" + Time.deltaTime);
+        Debug.Log("Update is calling" + Time.deltaTime);
     }
     private void FixedUpdate()
     {
-        Debug.Log("Fixupdate" + Time.deltaTime);
+        Debug.Log("Fixupdate is calling " + Time.deltaTime);
     }
+
+
+
 }
