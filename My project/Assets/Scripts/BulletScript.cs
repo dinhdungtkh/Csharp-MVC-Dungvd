f97e82f4cb533d1ddfe567a4e7e48debc7cde20b
DinDun <van.dinhdung.tkh@gmail.com>
2024-09-13 16:33:02 +0700

    Improving Game Performance with Object Pooling in Unity3d

--- My project/Assets/Scripts/BulletScript.cs
+++ My project/Assets/Scripts/BulletScript.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        transform.GetComponent<Rigidbody>().WakeUp();
+        Invoke("hideBullet", 2.0f);
+    }
+    void hideBullet()
+    {
+        gameObject.SetActive(false);
+    }
+    private void OnDisable()
+    {
+        transform.GetComponent<Rigidbody>().Sleep();
+        CancelInvoke();
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision" + collision.transform.name); 
+        Debug.Log("Collision" + collision.transform.name);
         if (collision.transform.tag == "enemy")
         {
             Destroy(collision.gameObject);
             gameObject.SetActive(false);
-        } 
+        }
     }
 }
--- My project/Assets/Scripts/Gun.cs
+++ My project/Assets/Scripts/Gun.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
-{ 
-     
+{
+
     [SerializeField]
     private GameObject bullet;
     [SerializeField]
@@ -13,19 +13,45 @@ public class Gun : MonoBehaviour
     private float bulletSpeed = 1000f;
     [SerializeField]
     private AudioSource audioSource;
+
+    List<GameObject> bulletList;
+
+    void Start()
+    {
+        bulletList = new List<GameObject>();
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject objBullet = (GameObject)Instantiate(bullet);
+            objBullet.SetActive(false);
+            bulletList.Add(objBullet);
+        }
+
+
+    }
     // Start is called before the first frame update
     void Fire()
     {
-        GameObject bullet = Instantiate(this.bullet, spawnPoint.position, spawnPoint.rotation);
-        bullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward.normalized * bulletSpeed, ForceMode.Impulse);  
-        Destroy(bullet, 2.0f);
-
+        for (int i = 0; i < bulletList.Count; i++)
+        {
+            if (!bulletList[i].activeInHierarchy)
+            {
+                bulletList[i].transform.position = spawnPoint.position;
+                bulletList[i].transform.rotation = spawnPoint.rotation;
+                bulletList[i].SetActive(true);
+                Rigidbody tempRigidBodyBullet = bulletList[i].GetComponent<Rigidbody>();
+                tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
+                
+                break;
+            }       
+        }
+        audioSource.Play();
     }
-    private void Fire2(){
+    private void Fire2()
+    {
         GameObject tempBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation) as GameObject;
         Rigidbody tempRigidBodyBullet = tempBullet.GetComponent<Rigidbody>();
         tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
-        Destroy(tempBullet, 1.0f); 
+        Destroy(tempBullet, 1.0f);
 
         audioSource.Play();
     }
@@ -33,10 +59,11 @@ public class Gun : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Fire2();
+           // Fire2();
+           Fire();
         }
     }
     // Update is called once per frame
 
-    
+
 }
