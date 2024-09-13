03dea351a62ab1927d5a75519a4c4df6093f863c
DinDun <van.dinhdung.tkh@gmail.com>
2024-09-13 13:42:35 +0700

    Rotate Camera with Mouse in Unity 3D
    + Khóa con trỏ chuột:
    + Theo dõi chuyển động chuột:
    + Giới hạn góc quay:
    +Áp dụng góc quay:

---
+++ My project/Assets/Scripts/CameraFollowMouse.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowMouse : MonoBehaviour
+{
+   public float speedHorizontal = 2.0f;
+   public float speedVertical = 2.0f;
+   public float smoothTime = 0.1f;
+
+   private float yaw = 0f;
+   private float pitch = 0f;
+   private Vector3 currentRotation;
+   private Vector3 rotationSmoothVelocity;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        currentRotation = transform.eulerAngles;
+    }
+
+    void Update()
+    {
+        yaw += speedHorizontal * Input.GetAxis("Mouse X");
+        pitch -= speedVertical * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+        Vector3 targetRotation = new Vector3(pitch, yaw, 0f);
+        currentRotation = Vector3.SmoothDamp(currentRotation, targetRotation, ref rotationSmoothVelocity, smoothTime);
+        transform.eulerAngles = currentRotation;
+    }
+}
